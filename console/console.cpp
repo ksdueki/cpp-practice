25bc493a7e29edec1d88e16f034c320070211262
ks-developer <ksapp@aok-net.com>
2019-06-21 17:34:49 +0900

    ソリューションの追加・テストプロジェクトの追加

--- console/console.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "pch.h"
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello World!\n";
-	int a;
-	std::cin >> a;
-}
---
+++ practice/console/console.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+// [A - Product](https://atcoder.jp/contests/abc086/tasks/abc086_a)
+/*
+問題文
+シカのAtCoDeerくんは二つの正整数 a,b を見つけました。
+a と b の積が偶数か奇数か判定してください。
+
+制約
+- 1≤a,b≤10000
+- a,b は整数
+入力
+入力は以下の形式で標準入力から与えられる。
+[a b]
+出力
+積が奇数なら [Odd] と、 偶数なら [Even] と出力せよ。
+
+入力例 1	[3 4]
+出力例 1	[Even]
+3×4=12 は偶数なので Even を出力してください。
+
+入力例 2	[1 21]
+出力例 2	[Odd]
+1×21=21 は奇数なので Odd を出力してください。
+*/
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	int a;
+	cin >> a;
+}
